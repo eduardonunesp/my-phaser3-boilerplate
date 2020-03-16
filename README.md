ad0b50cc4044a86f3f655b8c65424c3fdc827a4d
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2020-03-16 13:39:15 -0300

    My Phaser-3 boilerplate

---
+++ README.md
@@ -0,0 +1,14 @@
+#### Phaser Boilerplate
+
+# Install 
+
+```js
+npm install
+```
+
+# Run
+
+```js
+npm run dev
+```
+
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+import BootScene from './scenes/boot-scene'
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: [ BootScene ]
+}
+
+const game = new Phaser.Game(config)
+
